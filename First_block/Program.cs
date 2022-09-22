b7f723f03a06f98ef88a16c6bc39b23512c442d1
Elena Negerish <elenkaNeg@yandex.ru>
2022-09-22 15:32:40 +0300

    Create Array

---
+++ First_block/Program.cs
@@ -0,0 +1,10 @@
+﻿string[] CreateArray(int size)
+{
+    string[] newArray = new string[size];
+    for (int i = 0; i < newArray.Length; i++)
+    {
+        Console.WriteLine($"Input {i + 1} an array element: ");
+        newArray[i] = Convert.ToString(Console.ReadLine());
+    }
+    return newArray;
+}
